e5ed9ee7afc37590c064cc6b3ed84b614faded02
hyojin97S <gywls7948@naver.com>
2024-11-25 16:24:06 +0900

    parcel bundler project

---
+++ .postcssrc.js
@@ -0,0 +1,14 @@
+// node.js -> common.js
+module.exports = {
+  plugins : [
+    require('autoprefixer')
+  ]
+};
+
+// Es
+// import autoprefixer from 'autoprefixer';
+// export {
+//   plugin : [
+//     autoprefixer
+//   ]
+// }
---
+++ js/custom.js
@@ -0,0 +1,10 @@
+console.log('hello');
+const img = document.querySelector('.img');
+console.log(img);
+img.style.backgroundImage = 'url(./images/logo.png)';
+
+async function test() {
+  const promise = Promise.resolve(123);
+  console.log(promise)
+};
+test();
